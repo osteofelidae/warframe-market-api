2b86497e14964521e87d1fb93332269a988b972b
osteofelidae <osteofelidae@gmail.com>
2024-02-15 19:03:32 -0700

    Initial Commit

---
+++ src/wfm/__init__.py
@@ -0,0 +1,7 @@
+# === DEPENDENCIES =====================================================================================================
+# --- External dependencies --------------------------------------------------------------------------------------------
+import json  # JSON library is used to parse API calls
+import requests  # Requests library is used to make API calls
+
+# --- Internal dependencies --------------------------------------------------------------------------------------------
+from classes import *  # Classes for this library :)
