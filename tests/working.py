e893e75970d68cae842f38be8293cf423659a111
osteofelidae <osteofelidae@gmail.com>
2024-02-28 11:26:11 -0700

    Rename Item.fetch() to Item.update() Update Item.update() to be more consistent Add automatic item to blueprint checking in Item.update()
    Add Item.get_item_data() method
    Add appropriate unit tests
    
    Add Item.get_set_data() method
    Add appropriate unit tests

--- tests/working.py
+++ tests/working.py
@@ -1,4 +1,5 @@
 from src import wfm
+import json
 
 # TODO testing
 #wfm.auth.sign_up(email="osteofelidae@gmail.com",
@@ -6,5 +7,20 @@ from src import wfm
 
 i = wfm.Item()
 i._update_attributes({"url_name": "mirage_prime_systems"})
-i.fetch()
+#i._update_attributes({"url_name": "oull"})
+i.update()
 print(i.get_attributes())
+print(i.get_item_data())
+print(i.get_set_data())
+
+fp = open("temp.json", "w")
+json.dump(i.get_attributes(), fp, indent=4)
+fp.close()
+
+fp = open("temp2.json", "w")
+json.dump(i.get_item_data(), fp, indent=4)
+fp.close()
+
+fp = open("temp3.json", "w")
+json.dump(i.get_set_data(), fp, indent=4)
+fp.close()
